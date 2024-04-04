7b98cefae0680ada33abeeee2e3a16b8fca77e19
Rahulw2105 <wankhederahul116@gmail.com>
2024-04-05 04:26:44 +0530

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Home from "./Pages/Home.jsx";
 import "./index.css";
+
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const appRouter = createBrowserRouter([
