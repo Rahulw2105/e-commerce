a7eb55ef47ccaee4d9d250ff06974875eaf418d8
Rahulw2105 <wankhederahul116@gmail.com>
2024-05-05 02:28:13 +0530

    add home category page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import BannerSlider from "./components/Banners/BannerSlider";
+import HomeCategory from "./components/Category/HomeCategory";
 function App() {
   return (
     <>
       <Navbar />
+      <BannerSlider />
+      <HomeCategory />
       <Outlet />
     </>
   );
